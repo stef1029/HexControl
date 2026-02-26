4bb5ba1f295592c9408052d6619f2047de3ffea2
Stefan_RC <srr43@cam.ac.uk>
2026-02-26 13:31:03 +0000

    updated dlc submission script to be able to just take a video list instead of a cohort necessarily, and added readme for dlc scripts for usage help

--- behaviour_rig_system/main.py
+++ behaviour_rig_system/main.py
@@ -26,7 +26,7 @@ sys.path.insert(0, str(project_root))
 # =============================================================================
 
 # Path to the rig configuration file
-CONFIG_PATH = Path(r"C:\dev\projects\rigs.yaml")
+CONFIG_PATH = Path(r"/lmb/home/srogers/Dev/projects/hex_behav/hex_behav_control/behaviour_rig_system/config/rigs_template.yaml")
 
 
 # =============================================================================
