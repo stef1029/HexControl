b4bf00c45cadbb6d9d545f055f3feb362006fbba
Stefan Rogers-Coltman <srr43@cam.ac.uk>
2026-03-27 16:05:55 +0000

    fixed lock on rig window close

--- behaviour_rig_system/main.py
+++ behaviour_rig_system/main.py
@@ -27,8 +27,8 @@ sys.path.insert(0, str(project_root))
 
 # Path to the rig configuration file
 # CONFIG_PATH = Path(r"/lmb/home/srogers/Dev/projects/hex_behav/hex_behav_control/behaviour_rig_system/config/rigs_template.yaml")
-# CONFIG_PATH = Path(r"C:\Dev\projects\rigs_config.yaml") # (Small behaviour room)
-CONFIG_PATH = Path(r"C:\Dev\projects\hex_behav\hex_behav_control\behaviour_rig_system\config\rigs_template.yaml")
+CONFIG_PATH = Path(r"C:\Dev\projects\rigs_config.yaml") # (Small behaviour room)
+# CONFIG_PATH = Path(r"C:\Dev\projects\hex_behav\hex_behav_control\behaviour_rig_system\config\rigs_template.yaml")
 
 # CONFIG_PATH = Path(r"D:\Anindita\hex_behav_configs\rigs.yaml") # (Anindita - large behaviour room)
 # CONFIG_PATH = Path(r"D:\Yuanxin\hex_behav_configs\rigs.yaml") # (Yuanxin - large behaviour room)
